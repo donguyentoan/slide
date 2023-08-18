80e919bee6157fe8fe9a81c60522c91eebf731fa
taibeo <donguyentai05@gmail.com>
2023-08-18 21:43:48 +0700

    update

--- script.js
+++ script.js
@@ -4,17 +4,7 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
-        appendDots:$(".custom-slick-dots"),
         Accessibility: true,
     });
 
-    $('.custom-slick-arrow-1').on('click', function () {
-		$('.slick').slick('slickGoTo', 1);
-	});
-    $('.custom-slick-arrow-2').on('click', function () {
-		$('.slick').slick('slickGoTo', 2);
-	});
-    $('.custom-slick-arrow-3').on('click', function () {
-		$('.slick').slick('slickGoTo', 3);
-	});
   });
